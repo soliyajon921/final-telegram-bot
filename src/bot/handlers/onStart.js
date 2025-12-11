74d2a511a7b47be0fe06c92baf2e5529ba66bde2
soliyajon <@soliyajonbekturdiyeva83>
2025-12-11 14:14:56 +0500

    start buyrug'ini o'zgartirdim

--- src/bot/handlers/onStart.js
+++ src/bot/handlers/onStart.js
@@ -4,7 +4,7 @@ function onStart(msg) {
   const chatId = msg.chat.id;
   const firstname = msg.chat.first_name;
 
-  bot.sendMessage(chatId, `Assalomu aleykum, ${firstname}`);
+  bot.sendMessage(chatId, `Assalomu aleykum , ${firstname}😊, Xurmatli foydalanuvchi sizga qanday yordam bera olaman ? 🔥`);
 }
 
 export default onStart;
